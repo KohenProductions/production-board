131767251f6f7ecc2fb8c921f0dd336a38213ed9
ron kohen <ronkohen@Mac.lan>
2026-03-04 21:25:59 +0200

    fix vercel build for bcrypt

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    experimental: {
+      serverComponentsExternalPackages: ["bcrypt"],
+    },
+  };
+  
+  module.exports = nextConfig;
